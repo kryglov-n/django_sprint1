b57e860dbc67e3375e1f93958bb3de726d0efd0e
Kryglov Nick <n-kryglov@yandex.ru>
2023-12-28 14:46:38 +0300

    Fix template_dir

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blogicum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['blogicum/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
